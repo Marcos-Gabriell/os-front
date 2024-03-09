d9316d604f60b70ee3372cbc880d066d0bc7e859
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-03-09 11:45:47 -0300

    service  para os(ordem de serviços)

---
+++ src/app/services/os.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { OS } from '../models/OS';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OsService {
+
+  baseUrl: String = environment.baseUrl;
+
+  constructor(
+    private htpp : HttpClient,
+    private snack : MatSnackBar
+    ) { }
+
+  findAll():Observable<OS[]> {
+    const url = this.baseUrl + "/os";
+    return this.htpp.get<OS[]>(url);
+  }
+  
+  findById(id: any):Observable<OS> {
+    const url = `${this.baseUrl}/os/${id}`;
+    return this.htpp.get<OS>(url);
+  }
+  
+  create(os : OS):Observable<OS> {
+    const url = this.baseUrl + "/os";
+    return this.htpp.post<OS>(url, os);
+  }
+
+  update(os: OS): Observable<OS> {
+    const url = `${this.baseUrl}/os/${os.id}`;
+    return this.htpp.put<OS>(url, os);
+  }
+
+  delete(id : any): Observable<void> {
+    const url = `${this.baseUrl}/os/${id}`;
+    return this.htpp.delete<void>(url);
+  }
+
+  message(msg : string): void {
+    this.snack.open(`${msg}`, 'OK', {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 4000
+    })
+  }
+ }
