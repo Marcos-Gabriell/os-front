e3cc61607eb97c8b2a3f68825bdcc0e7c8bf6d5d
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-03-09 11:40:15 -0300

    model para os(ordem de serviços)

---
+++ src/app/models/OS.ts
@@ -0,0 +1,12 @@
+export interface OS{
+
+    id?: any;
+    dataAbertura?: any;
+    dataFechamento?: any;
+    prioridade: any;
+    observaçoes: String;
+    status: any;
+    tecnico: any;
+    clientw: any;
+    
+}
